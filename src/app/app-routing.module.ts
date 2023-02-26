1686cd670adce3595ea0b0801f7e26e27cb62b4b
Breno <brenocolettasartori@gmail.com>
2023-02-26 16:23:37 -0300

    content created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './pages/content/content.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+},
+// path vazio acessar o HomeComponent
+  {
+    path: 'content/:id',
+    component: ContentComponent
+  }
+  // path /content acessar o ContentComponent
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo": "https://i.pinimg.com/originals/e8/e7/92/e8e7920d482b22bfc1103b9b90c4dcf2.jpg",
+        "title": "Have you seen this wizard?",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor enim id massa rutrum, quis eleifend turpis bibendum. Nulla venenatis dignissim tellus, ac cursus arcu efficitur ut. Nunc in lacus in neque ornare convallis. Aenean at dictum justo. Proin tempus dictum nibh, quis dignissim sem feugiat at. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        "id": "2",
+        "photo": "https://i.pinimg.com/236x/60/96/86/609686dee350b06574393c827558291e--action-happy-birthday.jpg",
+        "title": "Dumbledore Daft or Dangerous?",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor enim id massa rutrum, quis eleifend turpis bibendum. Nulla venenatis dignissim tellus, ac cursus arcu efficitur ut. Nunc in lacus in neque ornare convallis. Aenean at dictum justo. Proin tempus dictum nibh, quis dignissim sem feugiat at. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        "id": "3",
+        "photo": "http://pm1.narvii.com/7689/b568a3fd20364f0925f527fb87fb3a3110e619a2r1-500-700v2_uhq.jpg",
+        "title": "New Headmaster for Hogwarts",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor enim id massa rutrum, quis eleifend turpis bibendum. Nulla venenatis dignissim tellus, ac cursus arcu efficitur ut. Nunc in lacus in neque ornare convallis. Aenean at dictum justo. Proin tempus dictum nibh, quis dignissim sem feugiat at. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        "id": "4",
+        "photo": "https://pm1.narvii.com/6841/71e6d2177517d679739de3bc1f5a59c02223f32bv2_hq.jpg",
+        "title": "The Boy Who Lies?",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor enim id massa rutrum, quis eleifend turpis bibendum. Nulla venenatis dignissim tellus, ac cursus arcu efficitur ut. Nunc in lacus in neque ornare convallis. Aenean at dictum justo. Proin tempus dictum nibh, quis dignissim sem feugiat at. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+]
+
+// banco de dados ficticio
